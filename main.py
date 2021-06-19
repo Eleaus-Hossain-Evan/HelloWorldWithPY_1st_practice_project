fdfac334fdf92e2b8771854cdebc459da9568e89
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2021-06-19 23:30:51 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+# print("Eleaus Hossain Evan")
+# print('o----')
+# print('||||')
+# print('*'*10)
+
+# # ------------------------------
+# # --------Exercise 1------------
+# # ------------------------------
+# name = input('What is your name? ')
+# color = input('What is your favourite color? ')
+#
+# print(name + ' likes '+ color)
+
+# # ------------------------------
+# # --------Exercise 2------------
+# # ------------------------------
+# weight_lbs = input('Weight (lbs): ')
+# weight_kg= int(weight_lbs) * .45
+# print(weight_kg)
+
+# # Formatted String
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'         #-------'f' need to use as prefix for formatted string
+# print(msg)
+
+# # String Methods
+# course = 'Python for Beginners'
+# len()
+# course.upper()
+# course.lower()
+# course.title()
+# course.find()
+# course.replace()
+# '...' in course
+
+# #Arithmetic Operations
+# print(10 / 3)         #---------return float value
+# print(10 // 3)        #---------return int value
+# print(10 ** 3)        #---------exponential (10^3)
+# x = (10 + 3) * 2 **2  #---------Operator precedence. 1st perenthesis
+#                                 #2nd exponentiation 3rd multiply or division &
+#                                 #4th addition or subsruction
+
+# #Math Fuctions
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))        #--------return postiove value
+#
+# import math             #--------Math Module [Math fuction]
+# print(math.ceil(2.6))
+# print(math.floor(2.6))
+
+# #IF Statements
+#
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clolthes")
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+
+# # ------------------------------
+# # --------Exercise 3------------
+# # ------------------------------
+
+# price = 1000000
+# has_good_credit = True
+#
+# if has_good_credit:
+#     down_payment = .1 * price
+# else:
+#     down_payment = .2 * price
+# print(f"Down Payment: ${down_payment}")
+
+# # ------------------------------
+# # ----------Car Game------------
+# # ------------------------------
+
